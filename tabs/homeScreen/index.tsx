93e4398680f92668bfca9571b55e8dd513f1f2eb
gab5987 <biell.gb4@gmail.com>
2022-07-29 15:10:40 -0300

    FIX. ios statusbar overlapping in dark mode

--- tabs/homeScreen/index.tsx
+++ tabs/homeScreen/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import { style } from '../styles'
 
 
 export function HomeScreen() {
     return (
       <View style={ style.tabDefaults }>
+        <StatusBar  barStyle="dark-content" translucent={true} />
         <Text> This will be the home screen... </Text>
         <Text> Stay tuned for changes! </Text>
       </View>
