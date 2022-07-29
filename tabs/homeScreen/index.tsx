e6c722e785bc78e00628731758280ae063d0a61c
gab5987 <biell.gb4@gmail.com>
2022-07-29 17:24:02 -0300

    FEAT. Add values description

--- tabs/homeScreen/index.tsx
+++ tabs/homeScreen/index.tsx
@@ -4,19 +4,51 @@ import BeautifulHorizontalList from "react-native-beautiful-horizontal-list";
 
 import { style } from '../styles'
 
+var recValues = {
+  vmed: 215.2,
+  vmax: 231.8,
+  cmed: 0.8,
+  cmax: 3.6
+}
+
+function colorPicker(value: number, type: string) {
+  if (type === 'voltage') {
+    if(value > 229 || value < 200) { return "#f73a4d" }
+    else { return "#16c402" }
+  } 
+  else {
+    if(value > 3) { return "#f73a4d" }
+    else if(value > 0.7){ return "#2d95f7" }
+    else { return "#16c402" }
+  }
+}
+
+function descPicker(value: number, type: string) {
+  if (type === 'voltage') {
+    if(value > 229) { return "Tensão Alta" }
+    else if(value < 200) { return "Tensão Baixa" }
+    else { return "Tensão Normal" }
+  }
+  else {
+    if(value > 3) { return "Corrente Crítica"}
+    else if(value > 0.7) { return "Alto Consumo"}
+    else { return "Baixo Consumo" }
+  }
+}
+
 const voltageArr = [
   {
     title: "Tensão Média",
-    value: "215",
-    unit: "Volts",
-    primaryColor: "#2d95f7",
+    value: recValues.vmed.toString() + " V",
+    unit: descPicker(recValues.vmed, "voltage"),
+    primaryColor: colorPicker(recValues.vmed, "voltage"),
     imageSource: require('../../assets/lightning.png')
   },
   {
     title: "Tensão Máxima",
-    value: "230",
-    unit: "Volts",
-    primaryColor: "#f73a2d",
+    value: recValues.vmax.toString() + " V",
+    unit: descPicker(recValues.vmax, "voltage"),
+    primaryColor: colorPicker(recValues.vmax, "voltage"),
     imageSource: require('../../assets/lightning.png')
   },
 ];
@@ -24,16 +56,16 @@ const voltageArr = [
 const currentArr = [
   {
     title: "Corrente Média",
-    value: "8,0",
-    unit: "Amperes",
-    primaryColor: "#2d95f7",
+    value: recValues.cmed.toString() + " A",
+    unit: descPicker(recValues.cmed, "current"),
+    primaryColor: colorPicker(recValues.cmed, "current"),
     imageSource: require('../../assets/current.png')
   },
   {
     title: "Corrente Máxima",
-    value: "15,6",
-    unit: "Amperes",
-    primaryColor: "#f73a2d",
+    value: recValues.cmax.toString() + " A",
+    unit: descPicker(recValues.cmax, "current"),
+    primaryColor: colorPicker(recValues.cmax, "current"),
     imageSource: require('../../assets/current.png')
   },
 ];
