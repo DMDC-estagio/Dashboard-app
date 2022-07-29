ae74e3572cc96e0f0fdd69caf92ed5c64a30c729
gab5987 <biell.gb4@gmail.com>
2022-07-29 15:08:26 -0300

    REMOVED. Unused modules

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import { LogsScreen } from './tabs/logsScreen'
 import { GroupScreen } from './tabs/groupScreen'
 import { HomeScreen } from './tabs/homeScreen';
+
 
 const Tab = createBottomTabNavigator();
 
--- tabs/styles/index.js
+++ tabs/styles/index.js
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import { StyleSheet } from 'react-native';
 
 export const style = StyleSheet.create({
