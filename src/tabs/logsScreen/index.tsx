cc622be90aaeddad82cd717b53e91d9ad08c6cf0
gab5987 <biell.gb4@gmail.com>
2022-07-30 14:51:53 -0300

    FEAT. Add list in logs screen

--- src/tabs/logsScreen/index.tsx
+++ src/tabs/logsScreen/index.tsx
@@ -1,22 +1,116 @@
-import * as React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StatusBar, Text, ScrollView } from 'react-native';
+import { ListItem, Avatar } from "@rneui/themed";
+
 import { style } from '../styles'
 
+const list = [
+  {
+    name: '2022-07-30 14:09:51',
+    subtitle: '221.7' + ' V',
+    sub2: '1.5' + ' A'
+  },
+  {
+    name: '2022-07-27 13:04:05',
+    subtitle: '219.2' + ' V',
+    sub2: '3.7' + ' A'
+  },
+  {
+    name: '2022-07-27 13:04:05',
+    subtitle: '219.2' + ' V',
+    sub2: '3.7' + ' A'
+  },
+  {
+    name: '2022-07-27 13:04:05',
+    subtitle: '219.2' + ' V',
+    sub2: '3.7' + ' A'
+  },
+  {
+    name: '2022-07-27 13:04:05',
+    subtitle: '219.2' + ' V',
+    sub2: '3.7' + ' A'
+  },
+  {
+    name: '2022-07-27 13:04:05',
+    subtitle: '219.2' + ' V',
+    sub2: '3.7' + ' A'
+  },
+  {
+    name: '2022-07-27 13:04:05',
+    subtitle: '219.2' + ' V',
+    sub2: '3.7' + ' A'
+  },
+  {
+    name: '2022-07-27 13:04:05',
+    subtitle: '219.2' + ' V',
+    sub2: '3.7' + ' A'
+  },
+  {
+    name: '2022-07-27 13:04:05',
+    subtitle: '219.2' + ' V',
+    sub2: '3.7' + ' A'
+  },
+  {
+    name: '2022-07-27 13:04:05',
+    subtitle: '219.2' + ' V',
+    sub2: '3.7' + ' A'
+  },
+  {
+    name: '2022-07-27 13:04:05',
+    subtitle: '219.2' + ' V',
+    sub2: '3.7' + ' A'
+  },
+  {
+    name: '2022-07-27 13:04:05',
+    subtitle: '219.2' + ' V',
+    sub2: '3.7' + ' A'
+  },
+  {
+    name: '2022-07-27 13:04:05',
+    subtitle: '219.2' + ' V',
+    sub2: '3.7' + ' A'
+  },
+];
+
+
+
 export function LogsScreen() {
-    return (
-      <View style={ style.tabDefaults }>
-        <Text> This will be the logs screen... </Text>
-        <Text> Stay tuned for changes! </Text>
+  const [isLoading, setLoading] = useState(true);
+  var [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://dmdc-stagging.herokuapp.com/api/logs')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if(isLoading){
+    return(
+    <View style={ style.loadingScreen }>
+        <StatusBar  barStyle="dark-content" translucent={true} />
+        <Text> Loading... </Text>
+    </View>
+    );
+  } else { console.log(data);
+    return(
+      <View>
+        <ScrollView>
+        {
+          list.map((l, i) => (
+            <ListItem key={i} bottomDivider>
+              <Avatar source={ require('../../assets/lightning.png') } />
+              <ListItem.Content>
+                <ListItem.Title>{l.name}</ListItem.Title>
+                <ListItem.Subtitle>{l.subtitle}</ListItem.Subtitle>
+                <ListItem.Subtitle>{l.sub2}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          ))
+        }
+        </ScrollView>
       </View>
     );
+  }
 }
-
-// const getData = async () => {
-//   try {
-//     var response = await fetch('https://dmdc-stagging.herokuapp.com/api/logs');
-//     var json = await Promise.resolve(response.json());
-//     console.log(json);
-//     return json
-//   } catch(err) { console.log(err); }
-// }
-// console.log(json.logs[0].voltageMed) { json.logs[0].voltageMed.toString() }
